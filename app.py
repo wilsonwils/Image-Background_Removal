914c836775dd1c726084898fb81896db729a8e46
Wilson <jesuwilson007@mail.com>
2025-07-11 17:10:25 +0530

    Fix: Bind Flask app to Render port

--- app.py
+++ app.py
@@ -87,5 +87,5 @@ def download_all():
     return send_file(zip_stream, mimetype='application/zip', as_attachment=True, download_name='processed_images.zip')  
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+     port = int(os.environ.get("PORT", 5000))
+     app.run(host="0.0.0.0", port=port,debug=False) 
